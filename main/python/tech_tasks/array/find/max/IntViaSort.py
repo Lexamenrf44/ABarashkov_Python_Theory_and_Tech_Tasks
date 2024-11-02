3c6f102b6a95e1a801690f54f10f5fd3290e878b
lexamenrf44 <alexander.stcstudent@gmail.com>
2024-11-02 23:30:06 +0100

    Added additional logic to the task, including sanity check

--- main/python/tech_tasks/array/find/max/IntViaSort.py
+++ main/python/tech_tasks/array/find/max/IntViaSort.py
@@ -1,5 +1,15 @@
 from main.python.helpers.DataTest import array
 
+print(f"This is what unsorted array looks like: {array}")
+
 array.sort()
 
-print(f"Maximum value is: {array[-1]}")
+print(f"This is what sorted array looks like: {array}")
+
+max_value = array[-1]
+
+if max_value == 9:
+    assert True
+    print(f"Test passed! Maximum value is: {max_value}")
+else:
+    assert False, f"Test failed! The maximum number is: {max_value}"
