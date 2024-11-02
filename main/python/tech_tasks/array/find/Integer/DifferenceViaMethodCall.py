97664a45f6aa701ec029fd58c54d0eb3043917ef
lexamenrf44 <alexander.stcstudent@gmail.com>
2024-11-02 23:25:52 +0100

    Dependencies fixes after method migration

--- main/python/tech_tasks/array/find/Integer/DifferenceViaMethodCall.py
+++ main/python/tech_tasks/array/find/Integer/DifferenceViaMethodCall.py
@@ -7,7 +7,9 @@ Find the difference between its maximum and minimum elements in array
 
 """
 
-difference = HelpMethods.calculate_array_difference(array)
+difference = HelpMethods.find_min_max_integer_range(array)
+
+print(f"{difference=}")
 
 if difference == 49:
     assert True
